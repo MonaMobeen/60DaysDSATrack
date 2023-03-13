5198bbcee65a084617e621c24318c18fc8b4e5e8
Memoona Mobeen <memoonamobeen612@gmail.com>
2023-03-13 13:19:37 +0000

    ZizZag

---
+++ Day20/Intersection.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> s(nums1.begin(), nums1.end());
+        vector<int> res;
+        for (int num : nums2) {
+            if (s.count(num)) {
+                res.push_back(num);
+                s.erase(num);
+            }
+        }
+        return res;
+    }
+};
